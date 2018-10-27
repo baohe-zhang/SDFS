00c4d107df78116941e79f5068192605925b84f7
baohe-zhang <baohez2@illinois.edu>
2018-10-27 16:45:55 -0500

    update Filesize

--- daemon/protocol.go
+++ daemon/protocol.go
@@ -8,12 +8,12 @@ const (
 
 type PutRequest struct {
 	Filename [128]byte
-	Filesize uint32
+	Filesize uint64
 }
 
 type PutResponse struct {
 	Filename     [128]byte
-	Filesize     uint32
+	Filesize     uint64
 	Timestamp    uint64
 	NexthopIP    uint32
 	NexthopPort  uint16
@@ -25,14 +25,14 @@ type PutAcknowledge struct {
 
 type WriteRequest struct {
 	Filename     [128]byte
-	Filesize     uint32
+	Filesize     uint64
 	Timestamp    uint64
 	DataNodeList [NumReplica]uint8
 }
 
 type WriteResponse struct {
 	Filename  [128]byte
-	Filesize  uint32
+	Filesize  uint64
 	Timestamp uint64
 	DataNode  uint8
 }
