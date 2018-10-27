dbc1967fd17d22b51c6d51d6371e586b64efbfa9
baohe-zhang <baohez2@illinois.edu>
2018-10-26 23:48:08 -0500

    Serialize and deserialize

---
+++ utils/utils.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"simpledfs/daemon"
+)
+
+func Serialize(data interface{}) []byte {
+	buf := bytes.Buffer{}
+	binary.Write(&buf, binary.BigEndian, data)
+	return buf.Bytes()
+}
+
+func Deserialize(data []byte, sample interface{}) {
+	buf := bytes.NewReader(data)
+	binary.Read(buf, binary.BigEndian, sample)
+}
+
+// Test client
+func main() {
+	putReq := daemon.PutRequest{
+		Filesize: uint32(10240),
+	}
+	copy(putReq.Filename[:], "myFilename")
+
+	b := Serialize(putReq)
+	fmt.Printf("%v", b)
+
+	s := daemon.PutRequest{}
+	Deserialize(b, &s)
+	fmt.Println(s.Filesize)
+}
