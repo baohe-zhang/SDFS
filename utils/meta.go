dc6d66e0d550d16b86af9b43572ae39ece68f3be
baohe-zhang <baohez2@illinois.edu>
2018-10-31 02:14:11 -0500

    json fotmati

--- utils/meta.go
+++ utils/meta.go
@@ -1,4 +1,4 @@
-package utils
+package main
 
 import (
 	"encoding/json"
@@ -7,14 +7,9 @@ import (
 	"os"
 )
 
-type Meta struct {
-	Files []File
-}
+type Meta map[string]Infos
 
-type File struct {
-	Filename string
-	Infos    []Info
-}
+type Infos []Info
 
 type Info struct {
 	Timestamp uint64
@@ -22,32 +17,49 @@ type Info struct {
 	DataNodes []uint8
 }
 
+func NewMeta(filename string) Meta {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+
+	meta := Meta{}
+	b, _ := ioutil.ReadAll(file)
+	json.Unmarshal(b, &meta)
+
+	return meta
+}
+
+func (meta Meta) FileInfo(filename string) Info {
+	return meta[filename][0]
+}
+
+func (meta Meta) PutFileInfo(filename string, info Info) {
+	meta[filename] = append(meta[filename], info)
+}
+
+func (meta Meta) SortFileInfo(filename string) {
+	
+}
+
+
+
 // Test client
-// func main() {
+func main() {
+	meta := NewMeta("meta3.json")
 
-// 	file, err := os.Open("meta.json")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	defer file.Close()
+	info := Info{
+		Timestamp: 20,
+		Filesize: 32,
+		DataNodes: []uint8{1,2,3,4},
+	}
 
-// 	var meta Meta
-// 	b, _ := ioutil.ReadAll(file)
-// 	json.Unmarshal(b, &meta)
+	meta.PutFileInfo("file1", info)
 
-// 	var target_info Info
+	fmt.Println(meta["file1"][3])
+}
 
-// 	// Get most up to date info
-// 	for _, file := range meta.Files {
-// 		if file.Filename == "file1" {
-// 			target_info = file.Infos[0]
-// 			for _, info := range file.Infos {
-// 				if info.Timestamp > target_info.Timestamp {
-// 					target_info = info
-// 				}
-// 			}
-// 			fmt.Println(target_info.Filesize)
-// 			break
-// 		}
-// 	}
-// }
+
+
+
