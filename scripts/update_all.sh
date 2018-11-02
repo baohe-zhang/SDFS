a8b94a48dc85c51d19fc51cc50cfe4966197bbd4
Kechen Lu <kechenl3@illinois.edu>
2018-11-01 19:26:26 -0500

    update
    Signed-off-by: Kechen Lu <kechenl3@illinois.edu>

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,7 +3,7 @@
 for val in 0{1..9} 10
 do
     echo VM$val
-    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/simpledfs; git pull; cd ./daemon; go build; exit"
+    ssh kechenl3@fa18-cs425-g29-$val.cs.illinois.edu "cd ~/go/src/simpledfs; git pull; cd ./daemon ; go build; exit"
 done
 echo 'Git Update!'
 
