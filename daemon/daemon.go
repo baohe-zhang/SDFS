97c4ab8b0069351d0b40b7b6dd77af5074328abd
baohe-zhang <baohez2@illinois.edu>
2018-11-04 18:37:55 -0600

    election

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -15,7 +15,11 @@ func detectNodeFailure(elector *election.Elector, tch chan uint64, ich chan uint
 		select {
 		case <-tch:
 			ip := <-ich
-			fmt.Printf("node %s with failed\n", utils.StringIP(ip))
+			fmt.Printf("node %s failed\n", utils.StringIP(ip))
+			if ip == utils.BinaryIP("172.22.156.95") {
+				fmt.Printf("master failed. election start")
+				elector.election()
+			}
 		default:
 		}
 	}
